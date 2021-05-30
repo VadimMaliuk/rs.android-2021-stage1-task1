951882b0b85501bff2712cb9af93be7eadef730c
VadimMaliuk <82815415+VadimMaliuk@users.noreply.github.com>
2021-05-30 15:12:30 +0300

    Add files via upload

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,40 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        //throw NotImplementedError("Not implemented")
+        var newArray = sadArray
+        return if (newArray.size>2) {
+            do {
+                var sadCount = 0
+                for (i in 1 until newArray.lastIndex) {
+                    if (newArray[i - 1] + newArray[i + 1] < newArray[i]) {
+                        sadCount++
+                    }
+                }
+                if (sadCount > 0) {
+                    newArray = cleaning(newArray, sadCount)
+                }
+            } while (sadCount > 0)
+
+            (newArray)
+        } else {
+            (sadArray)
+        }
+    }
+    private fun cleaning(sadArray: IntArray, sadCount: Int): IntArray{
+        val sizeofSadArray = sadArray.size - 2
+        val sizeOfHappyArray = sizeofSadArray + 2 - sadCount
+        val happyArray1 = IntArray(sizeOfHappyArray)
+        happyArray1[0] = sadArray[0]
+        happyArray1[sizeOfHappyArray - 1] = sadArray[sizeofSadArray + 1]
+        var p = 0
+        for (i in 1..sizeofSadArray) {
+            if (sadArray[i - 1] + sadArray[i + 1] < sadArray[i]) {
+                p++
+            } else {
+                happyArray1[i - p] = sadArray[i]
+            }
+        }
+        return (happyArray1)
     }
 }
